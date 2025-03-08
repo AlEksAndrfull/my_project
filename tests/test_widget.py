c60b7d2862464f6cea4009b6d8bcce36fdf1dba7
Aleksandr Novak <aleksandr@Noutbuk-Aleksandr.local>
2025-03-08 15:49:15 +0700

    Добавлены тесты для маскировки счета и карты в модуле widget

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,17 +1,48 @@
-import unittest
-from src.widget import mask_account_card, get_date
+import pytest
+
+from src.widget import get_date, mask_account_card
 
 
-class TestWidget(unittest.TestCase):
+# Фикстура для создания входных данных
+@pytest.fixture
+def account_data() -> list[tuple[str, str]]:
+    return [
+        ("Visa Platinum 7000792289606361", "7000 79** **** 6361"),
+        ("Maestro 1596837868705199", "1596 83** **** 5199"),
+        ("Счет 73654108430135874305", "**4305"),
+        ("MasterCard 7158300734726758", "7158 00** **** 6758"),
+        ("Visa Classic 6831982476737658", "6831 98** **** 7658"),
+        ("Visa Platinum 8990922113665229", "8990 22** **** 2229"),
+        ("Счет 64686473678894779589", "**9589"),
+        ("Счет 35383033474447895560", "**7860"),
+    ]
 
-    def test_mask_account_card(self):
-        self.assertEqual(mask_account_card("Visa Platinum 7000792289606361"), "Visa Platinum 7000 79** **** 6361")
-        self.assertEqual(mask_account_card("Счет 73654108430135874305"), "Счет **4305")
+
+@pytest.mark.parametrize("input_data, expected_output", [
+    ("Visa Platinum 7000792289606361", "7000 79** **** 6361"),
+    ("Maestro 1596837868705199", "1596 83** **** 5199"),
+    ("Счет 73654108430135874305", "**4305"),
+])
+def test_mask_account_card(account_data: list[tuple[str, str]], input_data: str, expected_output: str) -> None:
+    assert mask_account_card(input_data) == expected_output
 
 
-    def test_get_date(self):
-        self.assertEqual(get_date("2024-03-11T02:26:18.671407"), "11.03.2024")
+def test_mask_account_card_invalid() -> None:
+    with pytest.raises(ValueError):
+        mask_account_card(" ")  # строка с пробелом
+    with pytest.raises(ValueError):
+        mask_account_card("")  # пустая строка
+    with pytest.raises(ValueError):
+        mask_account_card("Некорректный ввод")  # строка без номера карты
+    with pytest.raises(ValueError):
+        mask_account_card("Счет 123")  # некорректный номер счета
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.mark.parametrize("date_str, expected_output", [
+    ("2024-03-11T02:26:18.671407", "11.03.2024"),
+    ("2018-07-11T00:00:00", "11.07.2018"),
+    ("", ""),  # Проверяем пустую строку
+    ("Некорректная дата", ""),  # Проверяем некорректный ввод
+])
+def test_get_date(date_str: str, expected_output: str) -> None:
+    assert get_date(date_str) == expected_output
